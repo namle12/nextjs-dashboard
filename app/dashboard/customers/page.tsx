7fec0ca145240ba8e87682645ee704e8809ac369
nam le <namsle2004@gmail.com>
2026-06-23 13:29:34 +0700

    Update invoices

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,13 @@
+import CustomersTable from "@/app/ui/customers/table";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Customers",
+};
 export default function Page() {
-  return <p>Customers Page</p>;
+  return (
+    <div>
+      <CustomersTable customers={[]} />
+    </div>
+  );
 }
